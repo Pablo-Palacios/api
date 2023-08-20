45c6cea5447ff772faac5ec6cfb74ccb5ca4e487
Pablo Palacios <pablomartinpalacios27@gmail.com>
2023-08-20 14:52:06 -0300

    se agregaron funciones test

--- test_API.py
+++ test_API.py
@@ -18,6 +18,8 @@ def test_can_create_task():
     assert create_task_response.status_code == 200
 
 
+
+
 def test_can_get_task():
     task = playload_task()
     create_task_response = create_task(task)
@@ -27,7 +29,61 @@ def test_can_get_task():
 
     #print(data)
     task_id = data ["task"]["task_id"]
-    get_task
+    get_task_reponse = get_task(task_id)
+    assert get_task_reponse.status_code == 200 
+
+
+def test_can_update_task():
+    task = playload_task()
+    create_task_response = create_task(task)
+    data = create_task_response.json()
+    task_id = data ["task"]["task_id"]
+    assert create_task_response.status_code == 200
+
+    new_playload = {
+                    "content": "my content update",
+                    "user_id": task["user_id"],
+                    "task_id": task_id,
+                    "is_done": True
+                                        }
+    
+    update_task_response = update_task(new_playload)
+    print(new_playload)
+    assert update_task_response.status_code == 200 
+
+
+def test_can_take_list_tasks():
+    n = 5
+    task = playload_task()
+    for _ in range(n):
+        create_task_response = create_task(task)
+        assert create_task_response.status_code == 200
+
+    user_id = task["user_id"]
+    list_tasks_response = list_tasks(user_id)
+    assert list_tasks_response.status_code == 200 
+
+    data = list_tasks_response.json()
+    print(data)
+
+    tasks = data["tasks"]
+    assert len(tasks) == n 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
 
@@ -52,4 +108,13 @@ def create_task(playload):
     return requests.put(HTTP + "/create-task", json= playload)
 
 def get_task(task_id):
-    return requests.get(HTTP + f"/get-task{task_id}")
+    return requests.get(HTTP + f"/get-task/{task_id}")
+
+
+
+def update_task(playload):
+    return requests.put(HTTP + "/update-task", json= playload)
+
+
+def list_tasks(user_id):
+    return requests.get(HTTP + f"/list-tasks/{user_id}")
